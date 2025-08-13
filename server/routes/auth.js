3bbddc9c88ad2957bf323ee51b4f7d519f9107d6
samkiel488e <samuelezekiel488@gmail.com>
2025-08-13 14:07:19 +0100

    Remove login rate limiting middleware from authentication route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { login } = require('../controllers/authController');
 
-const rateLimit = require('express-rate-limit');
-const loginLimiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 10 });
-router.post('/login', loginLimiter, login);
+router.post('/login', login);
 
 module.exports = router; 
